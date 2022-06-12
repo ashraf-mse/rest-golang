69e65172f3c934ba1120a21f19da71cfbf0ddd16
Ashraf Souf Eljeen <ashraf.souf@gmail.com>
2022-06-12 15:33:51 +0200

    all_crud

--- apis.go
+++ apis.go
@@ -8,10 +8,32 @@ import (
 
 func Posts(c *gin.Context) {
 
+	// limit = c.DefaultQuery("limit","15")
+	// offset := c.DefaultQuery("offset","0")
+	var posts []Post
+	db.Limit(15).Offset(0).Find(&posts)
+	c.JSON(http.StatusAccepted, gin.H{
+		"error": "",
+		"data":  posts,
+	})
 }
 
 func Show(c *gin.Context) {
 
+	id := c.Param("id")
+	var post Post
+	db.First(&post, id)
+	if post.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Post Not Found",
+			"data":  "",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"error": "",
+		"data":  post,
+	})
 }
 
 func Store(c *gin.Context) {
@@ -36,5 +58,19 @@ func Update(c *gin.Context) {
 }
 
 func Delete(c *gin.Context) {
-
+	id := c.Param("id")
+	var post Post
+	db.First(&post, id)
+	if post.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Post Not Found",
+			"data":  "",
+		})
+		return
+	}
+	db.Delete(&post)
+	c.JSON(http.StatusOK, gin.H{
+		"error": "",
+		"data":  "Post Deleted",
+	})
 }
